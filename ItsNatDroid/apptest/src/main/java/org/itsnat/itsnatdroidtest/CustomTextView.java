a4598a45a53610257e1042f0ccaff958ac15b223
josemaria.arranz <josemaria.arranz@bqreaders.com>
2014-07-22 21:31:44 +0200

    Changed project structure in Android Studio

---
+++ ItsNatDroid/apptest/src/main/java/org/itsnat/itsnatdroidtest/CustomTextView.java
@@ -0,0 +1,25 @@
+package org.itsnat.itsnatdroidtest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by jmarranz on 20/06/14.
+ */
+public class CustomTextView extends CustomTextViewBase
+{
+    public CustomTextView(Context context)
+    {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+}
