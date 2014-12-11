2c90e7c214d65ecbe0174ed31c62c1a5b10093cd
josemaria.arranz <josemaria.arranz@bqreaders.com>
2014-12-11 10:36:15 +0100

    Minor improvement for Opera Mini

--- itsnat_dev/src/java/org/itsnat/impl/comp/inplace/ItsNatHTMLCellEditorClientOperaOldMiniImpl.java
+++ itsnat_dev/src/java/org/itsnat/impl/comp/inplace/ItsNatHTMLCellEditorClientOperaOldMiniImpl.java
@@ -41,8 +41,7 @@ public class ItsNatHTMLCellEditorClientOperaOldMiniImpl extends ItsNatCellEditor
     {
         super.registerEventListeners(compParent,clientDocDeleg);
 
-        // Opera Mini por lo menos las versiones 4.0-4.2 no reciben clicks
-        // a nivel de documento, por ello añadimos además el siguiente truco.
+        // Opera Mini no recibe clicks a nivel de documento, por ello añadimos además el siguiente truco.
         // El definir el z-index no vale para nada en Opera Mini (el position:absolute sí) pero con así también funcionaría con FireFox etc
         // por ello lo dejamos por si en futuras versiones fuera más estándar el Opera Mini.
         // Aunque Opera Mini permite editar controles form distintos al editor inplace aunque haya un layer en teoría "encima" (ya sabemos que el z-index es ignorado)
@@ -50,7 +49,13 @@ public class ItsNatHTMLCellEditorClientOperaOldMiniImpl extends ItsNatCellEditor
         // antes de que se ejecute el change. Si se detectara algún caso en que no fuera así
         // podríamos usar un event listener global a nivel de documento en el servidor para quitar
         // el editor antes de procesar el evento.
+        // No es necesario código de "desregistro" porque si el editor
+        // se ha eliminado por otra vía el editor.dispatchEvent() no hace nada y el registro de DOMNodeRemovedFromDocument
+        // obviamente se pierde (pues el elemento en el cliente no se reutiliza de acuerdo con las reglas de ItsNat aunque no sea así en el servidor)
+        // y al ejecutarse el mutation se elimina el layer de utilidad en cualquier caso.        
 
+        // El recibir eventos a nivel de documento es útil para salir del edit inplace por ejemplo        
+        
         StringBuilder code = new StringBuilder();
         String method = "operaMiniInitInplaceEditor";
         if (!clientDocDeleg.isClientMethodBounded(method))
@@ -61,9 +66,9 @@ public class ItsNatHTMLCellEditorClientOperaOldMiniImpl extends ItsNatCellEditor
             code.append( "  var layer = itsNatDoc.doc.createElement('span');" );
             code.append( "  var zIndex = 999999;");
             code.append( "  layer.setAttribute('style','position:absolute; z-index:' + zIndex + '; margin:0; padding:0; top:0; left:0; width:' + topElem.scrollWidth + 'px; height:' + topElem.scrollHeight + 'px;');" ); // Para testeo:  border:3px solid blue;
-            code.append( "  itsNatDoc.doc.body.appendChild(layer);" );
             code.append( "  editor.style.position = 'absolute';" );
-            code.append( "  editor.style.zIndex = zIndex + 1;" );
+            code.append( "  editor.style.zIndex = zIndex + 1;" );         
+            code.append( "  itsNatDoc.doc.body.appendChild(layer);" );             
             code.append( "  var listener = function (evt)" );
             code.append( "  {" );
             code.append( "    if (evt.target != editor)" );
@@ -73,7 +78,7 @@ public class ItsNatHTMLCellEditorClientOperaOldMiniImpl extends ItsNatCellEditor
             code.append( "       editor.dispatchEvent(evtTmp);" );
             code.append( "    }" );
             code.append( "  };" );
-            code.append( "  layer.setAttribute('onclick','');" ); // Esto es para evitar el error de Opera Mini de los listeners-atributos
+            code.append( "  layer.setAttribute('onclick','');" ); // Esto es para evitar el error de Opera Mini de los listeners-atributos            
             code.append( "  layer.addEventListener('click',listener,true);" );
             code.append( "  var listener = function (evt)" );
             code.append( "  {" );
@@ -94,10 +99,5 @@ public class ItsNatHTMLCellEditorClientOperaOldMiniImpl extends ItsNatCellEditor
         code.append( "itsNatDoc." + method + "(nodeEditor);\n" );
 
         clientDocDeleg.addCodeToSend(code.toString());
-
-        // No es necesario código de "desregistro" porque si el editor
-        // se ha eliminado por otra vía el editor.dispatchEvent() no hace nada y el registro de DOMNodeRemovedFromDocument
-        // obviamente se pierde (pues el elemento en el cliente no se reutiliza de acuerdo con las reglas de ItsNat aunque no sea así en el servidor)
-        // y al ejecutarse el mutation se elimina el layer de utilidad en cualquier caso.
     }
 }
